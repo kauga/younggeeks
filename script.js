388fc41a1b4e009001e0158ca5808de330d744d8
kauga <alexlucas1944@gmail.com>
2024-10-02 14:20:27 +0300

    trial

--- script.js
+++ script.js
@@ -3,16 +3,16 @@
 const header = document.querySelector(".mainHeader");
 const menuOpen = document.querySelector(".menuOpenButton");
 const menuClose = document.querySelector(".menuCloseButton");
-const removeHeder = document.querySelector('.link');
+const removeHeder = document.querySelector(".link");
 const sectionObserver = document.querySelector(".header-section");
 
 // Navigation Menu
-menuOpen.addEventListener('click', function() {
-    console.log('menuOpen');
-    header.classList.add('hidden');
-    console.log('hell')
+menuOpen.addEventListener("click", function () {
+  console.log("menuOpen");
+  header.classList.add("hidden");
+  console.log("hell");
 });
 
-menuClose.addEventListener('click', function () {
-    header.classList.remove('hidden');
+menuClose.addEventListener("click", function () {
+  header.classList.remove("hidden");
 });
